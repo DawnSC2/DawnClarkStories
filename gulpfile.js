8e422f772f7a1824a28b8b4b3ba0593d8faabbff
DawnSC2 <clarkdaw2@gmail.com>
2024-07-18 10:07:04 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
-const { src, dest, parallel } = require('gulp'); // Removed 'watch' from here
+const { src, dest, parallel } = require('gulp');
 const eslint = require('gulp-eslint');
 const prettier = require('gulp-prettier');
+const stylelint = require('gulp-stylelint');
 
-function lint() {
+function lintJS() {
   return src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 }
 
-function format() {
-  return src(['**/*.js', '!node_modules/**'])
-    .pipe(prettier())
-    .pipe(dest((file) => file.base)); // Fixed Prettier error
+function lintCSS() {
+  return src(['**/*.css', '!node_modules/**'])
+    .pipe(stylelint({
+      reporters: [
+        { formatter: 'string', console: true }
+      ]
+    }));
 }
 
-exports.lint = lint;
-exports.format = format;
-exports.default = parallel(lint, format);
+function formatJS() {
+  return src(['**/*.js', '!node_modules/**'])
+    .pipe(prettier())
+    .pipe(dest((file) => file.base));
+}
+
+function formatCSS() {
+  return src(['**/*.css', '!node_modules/**'])
+    .pipe(prettier())
+    .pipe(dest((file) => file.base));
+}
+
+exports.lintJS = lintJS;
+exports.lintCSS = lintCSS;
+exports.formatJS = formatJS;
+exports.formatCSS = formatCSS;
+exports.lint = parallel(lintJS, lintCSS);
+exports.format = parallel(formatJS, formatCSS);
+exports.default = parallel(lintJS, lintCSS, formatJS, formatCSS);
