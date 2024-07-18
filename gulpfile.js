1ce4bc4e07cbc856be10b0ca6c362fe7b0c06562
DawnSC2 <clarkdaw2@gmail.com>
2024-07-18 02:27:20 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-const { src, dest, series, parallel, watch } = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 const eslint = require('gulp-eslint');
 const prettier = require('gulp-prettier');
 
 function lint() {
-  return src(['**/*.js', '!node_modules/**', '!build/**'])
+  return src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 }
 
 function prettify() {
-  return src(['**/*.js', '!node_modules/**', '!build/**'])
+  return src(['**/*.js', '!node_modules/**'])
     .pipe(prettier())
     .pipe(dest(file => file.base));
 }
 
 exports.lint = lint;
 exports.prettify = prettify;
-exports.default = series(lint, prettify);
+exports.default = parallel(lint, prettify);
